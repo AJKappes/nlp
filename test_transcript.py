f1ee7f0b619fe209872382f40c69bb6b662d21d3
AJKappes <alexander.j.kappes@gmail.com>
2021-12-23 21:28:20 -0800

    update with sentiment and rate data vis

--- test_transcript.py
+++ test_transcript.py
@@ -2,6 +2,8 @@ import glob
 import re
 import numpy as np
 import pandas as pd
+import matplotlib.pyplot as plt
+import random
 import nltk
 from nltk.sentiment.vader import SentimentIntensityAnalyzer
 from nltk.corpus import stopwords
@@ -30,7 +32,7 @@ def get_designated_text(file):
     
     return designated_text
 
-### speaker functions ###
+# speaker functions
 
 def get_speakers(file):
     
@@ -53,6 +55,19 @@ def get_speaker_text(num, file):
     
     return speaker_clean
 
+def get_speaker_rates(file):
+    
+    n_speakers = get_speakers(file)[1]
+    
+    speaker_text = []
+    for s in range(1, n_speakers + 1):
+        speaker_text.append(get_speaker_text(s, t_file))
+    
+    s_word_count = [len(''.join(s).split()) for s in speaker_text]
+    s_rates = [s_words/sum(s_word_count) for s_words in s_word_count]
+    
+    return s_rates
+
 def get_sen_df(s_text, speaker):
     
     s_df = pd.DataFrame(s_text, columns = [speaker])
@@ -68,57 +83,46 @@ def get_sen_df(s_text, speaker):
 
 # extractive summary 
 
-t_file = get_designated_text(raw_file)
-
-# sentences 
-s_trans = []
-for sen in t_file:
-    sen_i = re.sub(r'[^a-zA-Z]', ' ', sen)
-    s_trans.append(re.sub(r' Speaker   ', '', sen_i))
-
-# convert file to one full convo string
-convo = []
-for sen in t_file:
-    convo.append(re.sub(r'\[Speaker [0-9]\]', '', sen))
-convo = ''.join(convo)
-
-def sentence_sim(transcript_sentences):
+def get_summary(transcript_sentences, summary_n):
     
-    print('Computing sentence scores')
+    # compute sentence similarity matrix
+    # textrank through pagerank (cosine distance)
     
+    print('Computing sentence similarity')
     n = len(transcript_sentences)
-    sent_scores = np.zeros((n, n))
+    sent_sim = np.zeros((n, n))
     s_iter = 1
     
     for i in range(len(s_trans)):
         for j in range(len(s_trans)):
             if i != j:
-
+                
+                # i, j sentence comparison in nxn similarity matrix
                 sen_i = [w.lower() for w in s_trans[i].split()]
                 sen_j = [w.lower() for w in s_trans[j].split()]
                 w_diff = list(set(sen_i + sen_j))
                 w_dist_i = np.repeat(0, len(w_diff))
                 w_dist_j = np.repeat(0, len(w_diff))
-
+                
+                # index values for !stopwords
                 for w in sen_i:
                     if not w in stop_words:
-                        w_dist_i[w_diff.index(w)] += 1
+                        w_dist_i[w_diff.index(w)] = 1
 
                 for w in sen_j:
                     if not w in stop_words:
-                        w_dist_j[w_diff.index(w)] += 1
-
-                sent_scores[i, j] = 1 - cosine_distance(w_dist_i, w_dist_j)
+                        w_dist_j[w_diff.index(w)] = 1
+                
+                # similarity values
+                sent_sim[i, j] = 1 - cosine_distance(w_dist_i, w_dist_j)
                 print('   ' + str(s_iter) + ' of ' + str(n**2 - n) + ' complete')
                 s_iter += 1
-                
-    return sent_scores
-
-def get_summary(transcript_sentences, summary_n):
     
-    similarity = sentence_sim(transcript_sentences)
-    sent_scores = nx.pagerank(nx.from_numpy_array(similarity))
-
+    print('\nSentence similarity matrix complete\n')
+    
+    # extractive summary
+    
+    sent_scores = nx.pagerank(nx.from_numpy_array(sent_sim))
     score_val = list(sent_scores.values())
     score_key = list(sent_scores.keys())
     s_rank = sorted(score_val, reverse=True)[:summary_n]
@@ -133,18 +137,31 @@ def get_summary(transcript_sentences, summary_n):
         summary.append(transcript_sentences[key_val])
     
     text_summary = ' '.join(summary)
-    return text_summary
+    print(str(summary_n) + ' line summary extracted')
     
-get_summary(t_trans, 3)
+    return text_summary    
 
+########
 
+t_file = get_designated_text(raw_file)
 
+# convert file to one full convo string
+convo = []
+for sen in t_file:
+    convo.append(re.sub(r'\[Speaker [0-9]\]', '', sen))
+convo = ''.join(convo)
 
+# sentences 
+s_trans = []
+for sen in t_file:
+    sen_i = re.sub(r'[^a-zA-Z]', ' ', sen)
+    s_trans.append(re.sub(r' Speaker   ', '', sen_i))
 
-# other insights - sentiment over time, speaker time, etc
+get_summary(s_trans, 3)
+get_speaker_rates(t_file)
+
+########
                          
-
-
 # make dict with sent dfs
 speaker_dict = {}
 for s in get_speakers(t_file)[0]:
@@ -154,15 +171,34 @@ for s in get_speakers(t_file)[0]:
     speaker_text = get_speaker_text(speaker_val, t_file)
     speaker_dict[speaker] = get_sen_df(speaker_text, speaker)
 
-speaker_dict['Speaker_1']
-
 # plot sentiment over speaker time
 
+x = speaker_dict['Speaker_1'].index.tolist()
+sent_vals = [speaker_dict[s]['sent'].values for s in speaker_dict.keys()]
 
-# speaker rate
+fig, ax = plt.subplots()
+ax.plot(x, sent_vals[0], label = 'Speaker 1')
+ax.plot(x, sent_vals[1], label = 'Speaker 2')
+ax.set_ylabel('Sentiment')
+ax.set_ylim([-1, 1])
+ax.set_xticks([])
+ax.set_title('Individual Sentiment Across Meeting')
+plt.legend()
 
-s1 = get_speaker_text(1, t_file)
-s2 = get_speaker_text(2, t_file)
+# speaker rate vis
 
-s1_rate = len(''.join(s1).split())/(len(''.join(s1).split()) + len(''.join(s2).split()))
+speakers = get_speakers(t_file)[0]
+rates = get_speaker_rates(t_file)
+colors = ['#ff9999','#66b3ff']
 
+fig, ax = plt.subplots()
+ax.pie(rates, colors = colors, labels = speakers,
+       autopct = '%1.1f%%', startangle = 90, explode = (.01, .01))
+
+center_cir = plt.Circle((0,0), .85, fc = 'white')
+fig = plt.gcf()
+fig.gca().add_artist(center_cir)
+
+ax.axis('equal')
+plt.tight_layout()
+plt.show()
